a9310a0af3eb8beb98dea6665558a810d9c63f2f
MTCMarketing <slapkommerce@gmail.com>
2026-02-08 14:37:57 -0700

    Update sites.js

--- sites.js
+++ sites.js
@@ -1,2 +1,13 @@
-// UPDATED FROM CURL
-console.log("hello from supabase edge");
+window.MAIN_CONTENT = {
+  "SCHEMA" : 1, 
+  "VERSION" : 1,
+  "UNDER_CONSTRUCTION": false,
+  "ENABLE_EDITOR": false,
+  "ROADMAP_HIDE_RELATED_KEYWORDS": true,
+  "ROADMAP_HIDE_SOCIALS_ON_PROFILE": true,
+  "HIDE_CONTACT_ON_PUBLIC_PROFILE": true,
+  "HIDE_LANDING_VIDEO_SECTION": false,
+  "URL_REWRITING": true,
+  "DEFAULT_LANGUAGE": "vn",
+  "CONTENT" : null
+}; 
